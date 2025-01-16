2fd6ce07f968f223c7434a194d36011d82746bab
sidrah1998 <sidrah1998@hotmail.co.uk>
2025-01-15 19:56:40 -0500

    remove theme

--- src/containers/greeting/FeelingProud.js
+++ src/containers/greeting/FeelingProud.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 
 class FeelingProud extends Component {
   render() {
-    const theme = this.props.theme;
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
